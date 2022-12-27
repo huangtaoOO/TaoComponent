d9c9b0007927537568588dc9b4acae829f83b573
huangtaoOO <1462320178@qq.com>
2022-12-27 16:01:24 +0800

    fix bug

--- app/src/main/java/com/tao/taocomponent/MainActivity.kt
+++ app/src/main/java/com/tao/taocomponent/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.tao.taocomponent
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.lib_download.DownloadConfig
+import com.example.lib_download.DownloadListener
+import com.example.lib_download.DownloadStatus
+import com.example.lib_download.DownloadTask
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvProgress :TextView
+    private lateinit var btnDownload : Button
+    private lateinit var btnSuspend :Button
+
+    private val downloadTask by lazy {
+        val file = filesDir.absolutePath + File.separator + "download" + File.separator + "tim.exe"
+        DownloadTask(
+            "https://dldir1.qq.com/qqfile/qq/TIM3.4.3/TIM3.4.3.22064.exe",
+            file,
+            object : DownloadListener {
+                @SuppressLint("SetTextI18n")
+                override fun onDownloading(progress: Long, total: Long) {
+                    runOnUiThread {
+                        tvProgress.text = "下载进度：${progress}/${total}"
+                    }
+                }
+            })
+    }
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        DownloadConfig.setContext(application)
+
+        tvProgress = findViewById(R.id.tv_progress)
+        btnDownload = findViewById(R.id.btn_download)
+        btnSuspend = findViewById(R.id.btn_suspend)
+
+        btnDownload.setOnClickListener {
+            if (downloadTask.status == DownloadStatus.COMPLETED){
+                downloadTask.resetDownloadTask()
+            }else{
+                downloadTask.download()
+            }
+        }
+
+        btnSuspend.setOnClickListener {
+            downloadTask.pauseDownload()
+        }
     }
 }
--- lib-download/src/main/java/com/example/lib_download/DownloadDbImpl.kt
+++ lib-download/src/main/java/com/example/lib_download/DownloadDbImpl.kt
@@ -1,8 +1,10 @@
 package com.example.lib_download
 
 import android.content.ContentValues
+import android.util.Log
 import com.example.lib_download.db.DownloadDbOpenHelper
 import java.io.File
+import java.util.concurrent.Executor
 
 /**
  * Author: huangtao
@@ -16,113 +18,129 @@ class DownloadDbImpl : DownloadDbHelper {
     }
 
     override fun delete(task: SubDownloadTask) {
-        val writeDb = db.writableDatabase
-        val selection =
-            "${DownloadDbOpenHelper.FIELDS_URL} = ? AND ${DownloadDbOpenHelper.FIELDS_START_POS} = ?"
-        val selectionArgs = arrayOf(task.url, task.startPos.toString())
-        writeDb.delete(
-            DownloadDbOpenHelper.TABLE_NAME,
-            selection,
-            selectionArgs
-        )
+        try {
+            val writeDb = db.writableDatabase
+            val selection =
+                "${DownloadDbOpenHelper.FIELDS_URL} = ? AND ${DownloadDbOpenHelper.FIELDS_START_POS} = ?"
+            val selectionArgs = arrayOf(task.url, task.startPos.toString())
+            writeDb.delete(
+                DownloadDbOpenHelper.TABLE_NAME,
+                selection,
+                selectionArgs
+            )
+        } catch (e: Exception) {
+            Log.e(DownloadConfig.TAG, e.message, e)
+        }
     }
 
     override fun insert(task: SubDownloadTask) {
-        val writeDb = db.writableDatabase
-        val value = ContentValues().apply {
-            put(DownloadDbOpenHelper.FIELDS_URL, task.url)
-            put(DownloadDbOpenHelper.FIELDS_COMPLETE_SIZE, task.completeSize)
-            put(DownloadDbOpenHelper.FIELDS_TASK_SIZE, task.taskSize)
-            put(DownloadDbOpenHelper.FIELDS_SAVE_FILE, task.saveFile.absolutePath)
-            put(DownloadDbOpenHelper.FIELDS_START_POS, task.startPos)
-            put(DownloadDbOpenHelper.FIELDS_END_POS, task.endPos)
-            put(DownloadDbOpenHelper.FIELDS_CURRENT_POS, task.currentPos)
-            put(DownloadDbOpenHelper.FIELDS_STATUS, task.status.name)
+        try {
+            val writeDb = db.writableDatabase
+            val value = ContentValues().apply {
+                put(DownloadDbOpenHelper.FIELDS_URL, task.url)
+                put(DownloadDbOpenHelper.FIELDS_COMPLETE_SIZE, task.completeSize)
+                put(DownloadDbOpenHelper.FIELDS_TASK_SIZE, task.taskSize)
+                put(DownloadDbOpenHelper.FIELDS_SAVE_FILE, task.saveFile.absolutePath)
+                put(DownloadDbOpenHelper.FIELDS_START_POS, task.startPos)
+                put(DownloadDbOpenHelper.FIELDS_END_POS, task.endPos)
+                put(DownloadDbOpenHelper.FIELDS_CURRENT_POS, task.currentPos)
+                put(DownloadDbOpenHelper.FIELDS_STATUS, task.status.name)
+            }
+            writeDb.insert(DownloadDbOpenHelper.TABLE_NAME, null, value)
+        }catch (e : Exception){
+            Log.e(DownloadConfig.TAG, e.message, e)
         }
-        writeDb.insert(DownloadDbOpenHelper.TABLE_NAME, null, value)
     }
 
     override fun update(task: SubDownloadTask) {
-        val writeDb = db.writableDatabase
-        val value = ContentValues().apply {
-            put(DownloadDbOpenHelper.FIELDS_COMPLETE_SIZE, task.completeSize)
-            put(DownloadDbOpenHelper.FIELDS_TASK_SIZE, task.taskSize)
-            put(DownloadDbOpenHelper.FIELDS_SAVE_FILE, task.saveFile.absolutePath)
-            put(DownloadDbOpenHelper.FIELDS_END_POS, task.endPos)
-            put(DownloadDbOpenHelper.FIELDS_CURRENT_POS, task.currentPos)
-            put(DownloadDbOpenHelper.FIELDS_STATUS, task.status.name)
+        try {
+            val writeDb = db.writableDatabase
+            val value = ContentValues().apply {
+                put(DownloadDbOpenHelper.FIELDS_COMPLETE_SIZE, task.completeSize)
+                put(DownloadDbOpenHelper.FIELDS_TASK_SIZE, task.taskSize)
+                put(DownloadDbOpenHelper.FIELDS_SAVE_FILE, task.saveFile.absolutePath)
+                put(DownloadDbOpenHelper.FIELDS_END_POS, task.endPos)
+                put(DownloadDbOpenHelper.FIELDS_CURRENT_POS, task.currentPos)
+                put(DownloadDbOpenHelper.FIELDS_STATUS, task.status.name)
+            }
+
+            val selection =
+                "${DownloadDbOpenHelper.FIELDS_URL} = ? AND ${DownloadDbOpenHelper.FIELDS_START_POS} = ?"
+            val selectionArgs = arrayOf(task.url, task.startPos.toString())
+            writeDb.update(
+                DownloadDbOpenHelper.TABLE_NAME,
+                value,
+                selection,
+                selectionArgs
+            )
+        }catch (e : Exception){
+            Log.e(DownloadConfig.TAG, e.message, e)
         }
-
-        val selection =
-            "${DownloadDbOpenHelper.FIELDS_URL} = ? AND ${DownloadDbOpenHelper.FIELDS_START_POS} = ?"
-        val selectionArgs = arrayOf(task.url, task.startPos.toString())
-        writeDb.update(
-            DownloadDbOpenHelper.TABLE_NAME,
-            value,
-            selection,
-            selectionArgs
-        )
-
     }
 
     override fun queryByTaskTag(url: String): List<SubDownloadTask> {
-        val readDb = db.readableDatabase
-        val projection =
-            arrayOf(
-                DownloadDbOpenHelper.FIELDS_URL,
-                DownloadDbOpenHelper.FIELDS_TASK_SIZE,
-                DownloadDbOpenHelper.FIELDS_START_POS,
-                DownloadDbOpenHelper.FIELDS_END_POS,
-                DownloadDbOpenHelper.FIELDS_COMPLETE_SIZE,
-                DownloadDbOpenHelper.FIELDS_TASK_SIZE,
-                DownloadDbOpenHelper.FIELDS_STATUS,
-                DownloadDbOpenHelper.FIELDS_SAVE_FILE
+        try {
+            val readDb = db.readableDatabase
+            val projection =
+                arrayOf(
+                    DownloadDbOpenHelper.FIELDS_URL,
+                    DownloadDbOpenHelper.FIELDS_TASK_SIZE,
+                    DownloadDbOpenHelper.FIELDS_START_POS,
+                    DownloadDbOpenHelper.FIELDS_END_POS,
+                    DownloadDbOpenHelper.FIELDS_COMPLETE_SIZE,
+                    DownloadDbOpenHelper.FIELDS_CURRENT_POS,
+                    DownloadDbOpenHelper.FIELDS_STATUS,
+                    DownloadDbOpenHelper.FIELDS_SAVE_FILE
+                )
+
+            val selection = "${DownloadDbOpenHelper.FIELDS_URL} = ?"
+            val selectionArgs = arrayOf(url)
+
+            val sortOrder = "${DownloadDbOpenHelper.FIELDS_START_POS} DESC"
+
+            val cursor = readDb.query(
+                DownloadDbOpenHelper.TABLE_NAME,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                sortOrder
             )
 
-        val selection = "${DownloadDbOpenHelper.FIELDS_URL} = ?"
-        val selectionArgs = arrayOf(url)
-
-        val sortOrder = "${DownloadDbOpenHelper.FIELDS_START_POS} DESC"
-
-        val cursor = readDb.query(
-            DownloadDbOpenHelper.TABLE_NAME,
-            projection,
-            selection,
-            selectionArgs,
-            null,
-            null,
-            sortOrder
-        )
-
-        val result = mutableListOf<SubDownloadTask>()
-        with(cursor) {
-            while (moveToNext()) {
-                val sub = SubDownloadTask(
-                    getString(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_URL)),
-                    getLong(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_TASK_SIZE)),
-                    getLong(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_START_POS)),
-                    getLong(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_END_POS)),
-                    getLong(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_CURRENT_POS)),
-                    File(getString(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_SAVE_FILE))),
-                    null
-                ).apply {
-                    completeSize =
-                        getLong(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_COMPLETE_SIZE))
-                    val statusStr =
-                        getString(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_STATUS))
-                    status = when (statusStr) {
-                        DownloadStatus.IDLE.name -> DownloadStatus.IDLE
-                        DownloadStatus.DOWNLOADING.name -> DownloadStatus.DOWNLOADING
-                        DownloadStatus.PAUSE.name -> DownloadStatus.PAUSE
-                        DownloadStatus.ERROR.name -> DownloadStatus.ERROR
-                        DownloadStatus.COMPLETED.name -> DownloadStatus.COMPLETED
-                        else -> DownloadStatus.PAUSE
+            val result = mutableListOf<SubDownloadTask>()
+            with(cursor) {
+                while (moveToNext()) {
+                    val sub = SubDownloadTask(
+                        getString(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_URL)),
+                        getLong(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_TASK_SIZE)),
+                        getLong(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_START_POS)),
+                        getLong(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_END_POS)),
+                        getLong(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_CURRENT_POS)),
+                        File(getString(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_SAVE_FILE))),
+                        null
+                    ).apply {
+                        completeSize =
+                            getLong(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_COMPLETE_SIZE))
+                        val statusStr =
+                            getString(getColumnIndexOrThrow(DownloadDbOpenHelper.FIELDS_STATUS))
+                        status = when (statusStr) {
+                            DownloadStatus.IDLE.name -> DownloadStatus.IDLE
+                            DownloadStatus.DOWNLOADING.name -> DownloadStatus.DOWNLOADING
+                            DownloadStatus.PAUSE.name -> DownloadStatus.PAUSE
+                            DownloadStatus.ERROR.name -> DownloadStatus.ERROR
+                            DownloadStatus.COMPLETED.name -> DownloadStatus.COMPLETED
+                            else -> DownloadStatus.PAUSE
+                        }
                     }
+                    result.add(sub)
                 }
-                result.add(sub)
             }
+            cursor.close()
+            return result
+        }catch (e : Exception){
+            Log.e(DownloadConfig.TAG, e.message, e)
         }
-        cursor.close()
-        return result
+        return emptyList()
     }
 }
